f91a44ed8c97b79921a17e91b17e8d6f91f6fa38
jennifer <cxxyao2@gmail.com>
2023-09-21 17:48:04 -0400

    add memory card

--- app/components/WordMemoryCard.tsx
+++ app/components/WordMemoryCard.tsx
@@ -1,21 +1,27 @@
 'use client'
 
-import Image from 'next/image'
 import React from 'react'
+import { FiCheckSquare } from 'react-icons/fi'
 import styles from './WordMemoryCard.module.css'
 import { SafeNote } from '../types'
 
-type Props = { word: SafeNote }
+type Props = { word: SafeNote; frontColor: string; onClick: () => void }
 
-export default function WordMemoryCard({ word }: Props) {
+export default function WordMemoryCard({ word, frontColor, onClick }: Props) {
 	return (
 		<div className={styles.flipcard}>
 			<div className={styles.flipcardinner}>
-				<div className={styles.flipcardfront}>
+				<div className={`${styles.flipcardfront} ${frontColor}`}>
 					<p className={styles.centered}>{word?.keywords}</p>
 				</div>
 				<div className={styles.flipcardback}>
 					<p className={styles.centered}>{word?.content}</p>
+					<button
+						type='button'
+						onClick={onClick}
+						aria-label='Memorized this word'>
+						<FiCheckSquare className='absolute top-4 right-4 text-white-200 w-6 h-6'></FiCheckSquare>
+					</button>
 				</div>
 			</div>
 		</div>
--- app/memorize/MemoizeGrid.tsx
+++ app/memorize/MemoizeGrid.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { SafeNote } from '../types'
 import WordMemoryCard from '../components/WordMemoryCard'
@@ -6,16 +8,43 @@ type Props = {
 	newWords: SafeNote[] | null | undefined
 }
 
+const frontColors = [
+	'bg-blue-200',
+	'bg-green-200',
+	'bg-yellow-200',
+	'bg-red-200',
+	'bg-indigo-200',
+	'bg-purple-200',
+	'bg-pink-200',
+	'bg-neutral-200',
+	'bg-green-200'
+]
+
 export default function MemoizeGrid({ newWords }: Props) {
 	if (!newWords || newWords.length === 0) return null
+	// todo update array and database
+	const handleClick = (wordId: string) => {
+		console.log('wordId is', wordId)
+	}
 
 	return (
 		<div className='flex flex-col justify-center'>
-			<p> Need to memorize {newWords.length} words</p>
+			<p className='text-center m-4 text-2xl font-semifold'>
+				{' '}
+				Need to memorize{' '}
+				<span className='text-rose-800 dark:text-rose-200'>
+					{newWords.length}
+				</span>{' '}
+				words
+			</p>
 			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-				{newWords.slice(0, 9).map((word) => (
+				{newWords.slice(0, 9).map((word, index) => (
 					<div key={word.id} className='justify-self-center'>
-						<WordMemoryCard word={word} />
+						<WordMemoryCard
+							word={word}
+							frontColor={frontColors[index]}
+							onClick={() => handleClick(word.id)}
+						/>
 					</div>
 				))}
 			</div>
